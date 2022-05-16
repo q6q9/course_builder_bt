8ec6db8e204dd96149c52f51080bf73cdfc7e590
Evseev Maxim <evsvmx@gmail.com>
2022-05-16 11:40:18 +0000

    deploy

---
+++ _bridgetown/static/js/main.1b4d72bff7bf63551925.js
@@ -0,0 +1,2 @@
+(()=>{var o={213:o=>{function r(o){var r=new Error("Cannot find module '"+o+"'");throw r.code="MODULE_NOT_FOUND",r}r.keys=()=>[],r.resolve=r,r.id=213,o.exports=r}},r={};function e(t){var n=r[t];if(void 0!==n)return n.exports;var s=r[t]={exports:{}};return o[t](s,s.exports,e),s.exports}e.o=(o,r)=>Object.prototype.hasOwnProperty.call(o,r),(()=>{"use strict";const o=e(213);o.keys().forEach(o),console.info("Bridgetown is loaded!")})()})();
+//# sourceMappingURL=main.1b4d72bff7bf63551925.js.map
